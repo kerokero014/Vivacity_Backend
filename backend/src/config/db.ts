f13f7450bc705e8e2a70b04e7c5b75cecbaa749b
kerokero014 <kevinjrm.kr@gmail.com>
2024-07-02 19:31:40 -0600

    chore: Update npm dependencies and add dotenv to backend configuration

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,11 +1,12 @@
 import { Pool } from "pg";
+import dotenv from "dotenv";
 
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "personal",
-  password: "admin",
-  port: 5433,
+  user: process.env.DATASE_USER,
+  host: process.env.DATABASE_HOST,
+  database: process.env.DATABASE_NAME,
+  password: process.env.DATABASE_PASSWORD,
+  port: 5432,
 });
 
 export default pool;
