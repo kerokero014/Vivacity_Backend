20e2b25c94d3a1fbc6db8256970c8b6f95f98f2e
kerokero014 <kevinjrm.kr@gmail.com>
2024-07-03 00:37:40 -0600

    Refactor database configuration to use environment variables

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,12 +1,19 @@
-import { Pool } from "pg";
-import dotenv from "dotenv";
+// Removed unused import
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+console.log("DB_USER", process.env.DB_USER);
+console.log("DB_HOST", process.env.DB_HOST);
+
+const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: process.env.DATASE_USER,
-  host: process.env.DATABASE_HOST,
-  database: process.env.DATABASE_NAME,
-  password: process.env.DATABASE_PASSWORD,
-  port: 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PWD,
+  port: parseInt(process.env.DB_PORT as string, 10),
 });
 
 export default pool;
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,13 @@
 import express, { Request, Response } from "express";
 import pool from "./config/db";
 import mainRoutes from "./routes";
+import dotenv from "dotenv";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const cors = require("cors");
+
+dotenv.config();
 
 app.use(cors());
 app.use(express.json());
@@ -18,11 +21,11 @@ app.use("/", mainRoutes);
 app.listen(port, async () => {
   console.log(`Example app listening at http://localhost:${port}`);
 
-  // Test the database connection on server startup and send current date
+  // Test the database connection on server startup and send the current date
   try {
     const res = await pool.query("SELECT NOW()");
-    console.log("Database connected", res.rows[0]);
+    console.log("Database connected:", res.rows[0]);
   } catch (err: any) {
-    console.error("Database connection failed", err.stack);
+    console.error("Database connection failed:", err.stack);
   }
 });
